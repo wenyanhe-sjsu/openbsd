63117221bc355d13194cddfa5c8e792467397965
ruchikahazariwal <ruchika.hazariwal@sjsu.edu>
2020-02-20 16:33:57 -0800

    experiment

--- sys/dev/pv/viomb.c
+++ sys/dev/pv/viomb.c
@@ -31,9 +31,9 @@
 // #include <stdlib.h>
 // #include <string.h>
 // #include <time.h>
-#include <unistd.h>
+//#include <unistd.h>
 
-#include "../../../usr.bin/vmstat/dkstats.h"
+//#include "../../../usr.bin/vmstat/dkstats.h"
 #include "../../../usr.bin/systat/systat.h"
 
 
