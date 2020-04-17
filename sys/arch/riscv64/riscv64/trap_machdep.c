8fdf2f2ab7d3952da32e4cc2ca3a88db2668473b
Shivam Waghela <shivamwaghela@gmail.com>
2020-04-17 15:32:01 -0700

    Fix do_trap_user and some cleanup

--- sys/arch/riscv64/riscv64/trap_machdep.c
+++ sys/arch/riscv64/riscv64/trap_machdep.c
@@ -33,6 +33,8 @@
 void do_trap_supervisor(struct trapframe *);
 void do_trap_user(struct trapframe *);
 
+static void data_abort(struct trapframe *, int);
+
 static void
 dump_regs(struct trapframe *frame)
 {
@@ -72,7 +74,6 @@ do_trap_supervisor(struct trapframe *frame)
 
 	exception = (frame->tf_scause & EXCP_MASK);
 	switch(exception) {
-#if 0
 	case EXCP_FAULT_LOAD:
 	case EXCP_FAULT_STORE:
 	case EXCP_FAULT_FETCH:
@@ -80,7 +81,6 @@ do_trap_supervisor(struct trapframe *frame)
 	case EXCP_LOAD_PAGE_FAULT:
 		data_abort(frame, 0);
 		break;
-#endif
 	case EXCP_BREAKPOINT:
 #ifdef DDB
 		// kdb_trap(exception, 0, frame);
@@ -97,6 +97,83 @@ do_trap_supervisor(struct trapframe *frame)
 	default:
 		dump_regs(frame);
 		panic("Unknown kernel exception %x trap value %lx\n",
+		    exception, frame->tf_stval);
+	}
+}
+
+
+void
+do_trap_user(struct trapframe *frame)
+{
+	uint64_t exception;
+	union sigval sv; 
+	struct proc *p;
+	struct pcb *pcb;
+	uint64_t stval;
+
+	p = curcpu()->ci_curproc;
+	p->p_addr->u_pcb.pcb_tf = frame;
+	pcb = curcpu()->ci_curpcb; 
+
+	/* Ensure we came from usermode, interrupts disabled */
+	KASSERTMSG((csr_read(sstatus) & (SSTATUS_SPP | SSTATUS_SIE)) == 0,
+	    "Came from U mode with interrupts enabled");
+
+	exception = (frame->tf_scause & EXCP_MASK);
+	if (frame->tf_scause & EXCP_INTR) {
+		/* Interrupt */
+		riscv_cpu_intr(frame);
+		return;
+	}
+
+#if 0	// XXX Debug logging
+	CTR3(KTR_TRAP, "do_trap_user: curthread: %p, sepc: %lx, frame: %p",
+	    curthread, frame->tf_sepc, frame);
+#endif
+
+	switch(exception) {
+	case EXCP_FAULT_LOAD:
+	case EXCP_FAULT_STORE:
+	case EXCP_FAULT_FETCH:
+	case EXCP_STORE_PAGE_FAULT:
+	case EXCP_LOAD_PAGE_FAULT:
+	case EXCP_INST_PAGE_FAULT:
+		data_abort(frame, 1);
+		break;
+	case EXCP_USER_ECALL:
+		frame->tf_sepc += 4;	/* Next instruction */
+		svc_handler(frame);
+		break;
+	case EXCP_ILLEGAL_INSTRUCTION:
+#ifdef FPE // XXX
+		if ((pcb->pcb_fpflags & PCB_FP_STARTED) == 0) {
+			/*
+			 * May be a FPE trap. Enable FPE usage
+			 * for this thread and try again.
+			 */
+			fpe_state_clear();
+			frame->tf_sstatus &= ~SSTATUS_FS_MASK;
+			frame->tf_sstatus |= SSTATUS_FS_CLEAN;
+			pcb->pcb_fpflags |= PCB_FP_STARTED;
+			break;
+		}
+#endif
+		sv.sival_int = stval;
+		KERNEL_LOCK();
+		trapsignal(p, SIGILL, 0, ILL_ILLTRP, sv);
+		KERNEL_UNLOCK();
+		userret(p);
+		break;
+	case EXCP_BREAKPOINT:
+		sv.sival_int = stval;
+		KERNEL_LOCK();
+		trapsignal(p, SIGTRAP, 0, TRAP_BRKPT, sv);
+		KERNEL_UNLOCK();
+		userret(p);
+		break;
+	default:
+		dump_regs(frame);
+		panic("Unknown userland exception %x, trap value %lx\n",
 		    exception, frame->tf_stval);
 	}
 }
@@ -188,163 +265,4 @@ fatal:
 	dump_regs(frame);
 	panic("Fatal page fault at %#lx: %#016lx", frame->tf_sepc, sv);
 }
-
-void
-do_trap_user(struct trapframe *frame)
-{
-	uint64_t exception;
-	//union sigval sv; // XXX
-	struct proc *p;
-	// struct pcb *pcb; // XXX
-
-	p = curproc;
-	p->p_addr->u_pcb.pcb_tf = frame;
-	// pcb = td->td_pcb; // XXX
-
-	/* Ensure we came from usermode, interrupts disabled */
-	KASSERTMSG((csr_read(sstatus) & (SSTATUS_SPP | SSTATUS_SIE)) == 0,
-	    "Came from U mode with interrupts enabled");
-
-	exception = (frame->tf_scause & EXCP_MASK);
-	if (frame->tf_scause & EXCP_INTR) {
-		/* Interrupt */
-		riscv_cpu_intr(frame);
-		return;
-	}
-
-#if 0	// XXX Debug logging
-	CTR3(KTR_TRAP, "do_trap_user: curthread: %p, sepc: %lx, frame: %p",
-	    curthread, frame->tf_sepc, frame);
-#endif
-
-	switch(exception) {
-	case EXCP_FAULT_LOAD:
-	case EXCP_FAULT_STORE:
-	case EXCP_FAULT_FETCH:
-	case EXCP_STORE_PAGE_FAULT:
-	case EXCP_LOAD_PAGE_FAULT:
-	case EXCP_INST_PAGE_FAULT:
-		data_abort(frame, 1);
-		break;
-	case EXCP_USER_ECALL:
-		frame->tf_sepc += 4;	/* Next instruction */
-		svc_handler(frame);
-		break;
-#if 0
-	case EXCP_ILLEGAL_INSTRUCTION:
-#ifdef FPE
-		if ((pcb->pcb_fpflags & PCB_FP_STARTED) == 0) {
-			/*
-			 * May be a FPE trap. Enable FPE usage
-			 * for this thread and try again.
-			 */
-			fpe_state_clear();
-			frame->tf_sstatus &= ~SSTATUS_FS_MASK;
-			frame->tf_sstatus |= SSTATUS_FS_CLEAN;
-			pcb->pcb_fpflags |= PCB_FP_STARTED;
-			break;
-		}
-#endif
-		call_trapsignal(td, SIGILL, ILL_ILLTRP, (void *)frame->tf_sepc);
-		userret(td, frame);
-		break;
-	case EXCP_BREAKPOINT:
-		call_trapsignal(td, SIGTRAP, TRAP_BRKPT, (void *)frame->tf_sepc);
-		userret(td, frame);
-		break;
-#endif
-	default:
-		dump_regs(frame);
-		panic("Unknown userland exception %x, trap value %lx\n",
-		    exception, frame->tf_stval);
-	}
-}
-
-#if 0
-#include <sys/cdefs.h>
-
-#include <sys/param.h>
-#include <sys/systm.h>
-#include <sys/kernel.h>
-//#include <sys/ktr.h>
-#include <sys/lock.h>
-#include <sys/mutex.h>
-//#include <sys/pioctl.h>
-//#include <sys/bus.h>
-#include <sys/proc.h>
-#include <sys/ptrace.h>
-#include <sys/syscall.h>
-//#include <sys/sysent.h>
-#ifdef KDB
-#include <sys/kdb.h>
-#endif
-
-#ifdef FPE
-#include <machine/fpe.h>
-#endif
-#include <machine/frame.h>
-#include <machine/pcb.h>
-//#include <machine/pmap.h>
-
-//#include <machine/resource.h>
-#include <machine/intr.h>
-
-#ifdef KDTRACE_HOOKS
-#include <sys/dtrace_bsd.h>
-#endif
-
-int (*dtrace_invop_jump_addr)(struct trapframe *);
-
-extern register_t fsu_intr_fault;
-#endif
-
-#if 0
-static __inline void
-call_trapsignal(struct thread *td, int sig, int code, void *addr)
-{
-	ksiginfo_t ksi;
-
-	ksiginfo_init_trap(&ksi);
-	ksi.ksi_signo = sig;
-	ksi.ksi_code = code;
-	ksi.ksi_addr = addr;
-	trapsignal(td, &ksi);
-}
-
-int
-cpu_fetch_syscall_args(struct thread *td)
-{
-	struct proc *p;
-	register_t *ap;
-	struct syscall_args *sa;
-	int nap;
-
-	nap = NARGREG;
-	p = td->td_proc;
-	sa = &td->td_sa;
-	ap = &td->td_frame->tf_a[0];
-
-	sa->code = td->td_frame->tf_t[0];
-
-	if (sa->code == SYS_syscall || sa->code == SYS___syscall) {
-		sa->code = *ap++;
-		nap--;
-	}
-
-	if (sa->code >= p->p_sysent->sv_size)
-		sa->callp = &p->p_sysent->sv_table[0];
-	else
-		sa->callp = &p->p_sysent->sv_table[sa->code];
-
-	sa->narg = sa->callp->sy_narg;
-	memcpy(sa->args, ap, nap * sizeof(register_t));
-	if (sa->narg > nap)
-		panic("TODO: Could we have more then %d args?", NARGREG);
-
-	td->td_retval[0] = 0;
-	td->td_retval[1] = 0;
-
-	return (0);
-}
-#endif
 
